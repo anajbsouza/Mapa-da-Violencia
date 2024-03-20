9f3aa00f5e99f656a14c9fc6d0ba7358362bc8b9
anajbsouza <anaju.batsouza@gmail.com>
2024-03-20 11:01:38 -0300

    feat: frontend setup

--- Códigos/frontend/src/pages/_app.tsx
+++ Códigos/frontend/src/pages/_app.tsx
@@ -1,3 +1,10 @@
+/*
+O arquivo _app.tsx é usado para envolver todos os seus componentes de página 
+com uma camada de lógica de nível superior. Ele é carregado uma vez, no lado do cliente, 
+quando o aplicativo é inicializado, e é útil para configurar o layout comum, 
+adicionar estilos globais, adicionar provedores de contexto ou roteadores, entre outras coisas.
+*/
+
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
