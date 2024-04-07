6cb19962541f0377d46cbed0033ea0c6af1abffb
anajbsouza <anaju.batsouza@gmail.com>
2024-04-07 15:45:25 -0300

    fix: css style at about violence

--- Códigos/frontend/src/pages/FormAboutViolencePage.tsx
+++ Códigos/frontend/src/pages/FormAboutViolencePage.tsx
@@ -20,50 +20,45 @@ const FormAboutViolencePage = () => {
 
     return (
         <div>
-        <Header />
-            <section className="question">
-                    
-                <h3>Sinta-se a vontade para compartilhar conosco algumas informações sobre a violência que você enfrentou.</h3>
-
-                <div>
-                    <label htmlFor="dateInput">2. Que dia ocorreu a violência?</label>
-                    <input
-                        type="date"
-                        id="dateInput"
-                        value={date}
-                        onChange={(e) => setDate(e.target.value)}
-                    />
-                </div>
-
-                <div>
-                <label htmlFor="timeInput">3. Qual foi o horário do ocorrido?</label>
-                    <input
-                        type="time"
-                        id="timeInput"
-                        value={time}
-                        onChange={(e) => setTime(e.target.value)}
-                    />
-                </div>
-
-                <div>
-                <label htmlFor="ageRangeInput">4. Qual a sua faixa etária?</label>
-                <select
-                    id="ageRangeInput"
-                    value={ageRange}
-                    onChange={(e) => setAgeRange(e.target.value)}
-                    >
-                    {ageRangeOptions.map((option, index) => (
-                        <option key={index} value={option}>
-                        {option}
-                        </option>
-                    ))}
-                </select>
-                </div>
-
-                <div>
-                    <Footer nextPage="/form-classify-violence" />
-                </div>
-            </section>
+            <Header />
+            <main>
+                <section className="question">
+                    <h3>Sinta-se a vontade para compartilhar conosco algumas informações sobre a violência que você enfrentou.</h3>
+                    <div>
+                        <label htmlFor="dateInput">2. Que dia ocorreu a violência?</label>
+                        <input
+                            type="date"
+                            id="dateInput"
+                            value={date}
+                            onChange={(e) => setDate(e.target.value)}
+                        />
+                    </div>
+                    <div>
+                    <label htmlFor="timeInput">3. Qual foi o horário do ocorrido?</label>
+                        <input
+                            type="time"
+                            id="timeInput"
+                            value={time}
+                            onChange={(e) => setTime(e.target.value)}
+                        />
+                    </div>
+                    <div>
+                    <label htmlFor="ageRangeInput">4. Qual a sua faixa etária?</label>
+                    <select
+                        id="ageRangeInput"
+                        value={ageRange}
+                        onChange={(e) => setAgeRange(e.target.value)}
+                        >
+                        {ageRangeOptions.map((option, index) => (
+                            <option key={index} value={option}>
+                            {option}
+                            </option>
+                        ))}
+                    </select>
+                    </div>
+                </section>
+            </main>
+            <Footer nextPage="/form-classify-violence" />
         </div>
 
     );
