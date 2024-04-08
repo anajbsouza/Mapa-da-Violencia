7d33ddde69d270c3bef5fd30e32b962b2c5549a3
barbarabea <barbarabeatrizsilva6@gmail.com>
2024-04-08 09:57:25 -0300

    feat: adding pages

---
+++ Códigos/frontend/src/pages/WhatToDoPage.tsx
@@ -0,0 +1,12 @@
+import Footer from "../components/Footer";
+
+const WhatToDoPage = () => {
+    return (
+        <div>
+            <h1>Página o que fazer em breve</h1>
+            <Footer nextPage="/authorize-localization" />
+        </div>
+    );
+};
+
+export default WhatToDoPage;
