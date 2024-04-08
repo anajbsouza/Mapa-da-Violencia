d499e7829369246b81983a62fe894971c8c5208b
barbarabea <barbarabeatrizsilva6@gmail.com>
2024-04-08 09:57:25 -0300

    feat: adding pages

---
+++ Códigos/frontend/src/pages/ThankYouPage.tsx
@@ -0,0 +1,12 @@
+import Footer from "../components/Footer";
+
+const ThankYouPage = () => {
+    return (
+        <div>
+            <h1>Página de agradecimento em breve</h1>
+            <Footer nextPage="/authorize-localization" />
+        </div>
+    );
+};
+
+export default ThankYouPage ;
---
+++ Códigos/frontend/src/pages/ViolenceTypesPage.tsx
@@ -0,0 +1,12 @@
+import Footer from "../components/Footer";
+
+const ViolenceTypesPage = () => {
+    return (
+        <div>
+            <h1>Página tipos de violência em breve</h1>
+            <Footer nextPage="/authorize-localization" />
+        </div>
+    );
+};
+
+export default ViolenceTypesPage;
