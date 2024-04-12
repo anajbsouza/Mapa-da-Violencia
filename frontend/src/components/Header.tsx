b6328b8ff279b50d9c11e6db3bcb5590d6bbb5ed
barbarabea <barbarabeatrizsilva6@gmail.com>
2024-04-12 11:52:11 -0300

    editing header back button - header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,5 @@
 
 import Logo from "../assets/logo.png"
-import Botao from "../assets/button_back.png"
 import { useNavigate } from "react-router-dom";
 
 const Header = () => {
@@ -9,7 +8,13 @@ const Header = () => {
     return (
         <div>
             <header>
-                <img className="back" src={Botao} alt="Botão de voltar" onClick={() => navigate(-1)}/>
+                <button className="back">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" width="24px" height="24px">
+                        <path d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12l4.58-4.59z"/>
+                    </svg>
+                </button>
+                
                 <img className="logo" src={Logo} alt="Logo da Gloria" onClick={() => navigate('/')}/>
             </header>
         </div>
