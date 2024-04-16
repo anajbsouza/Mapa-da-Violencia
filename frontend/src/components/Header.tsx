3c480013f404e3e8e4b47a59a87f4c44f086f53f
Beatriz <bibi.geane2@gmail.com>
2024-04-16 13:51:09 -0300

    fix: adjusting conflicts

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,4 +1,3 @@
-
 import Logo from "../assets/logo.png"
 import { useNavigate } from "react-router-dom";
 
@@ -7,15 +6,19 @@ const Header = () => {
 
     return (
         <div>
-            <header>
-                <button className="back">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" width="24px" height="24px">
-                        <path d="M0 0h24v24H0z" fill="none"/>
-                        <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12l4.58-4.59z"/>
-                    </svg>
-                </button>
+            <header className="header">
+                <section className="button-back">
+                    <button className="back">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" width="24px" height="24px">
+                            <path d="M0 0h24v24H0z" fill="none"/>
+                            <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12l4.58-4.59z"/>
+                        </svg>
+                    </button>
+                </section>
                 
-                <img className="logo" src={Logo} alt="Logo da Gloria" onClick={() => navigate('/')}/>
+                <section className="button-logo">
+                    <img className="logo" src={Logo} alt="Logo da Gloria" onClick={() => navigate('/')}/>
+                </section>
             </header>
         </div>
     );
