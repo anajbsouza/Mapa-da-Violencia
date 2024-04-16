4cbde193e16d6990ae458d83eea63e1393b0195a
catlenc <catlen.cleane@hotmail.com>
2024-04-16 08:58:04 -0300

    configurações banco de dados

--- backend/src/repositories/answers-repository.ts
+++ backend/src/repositories/answers-repository.ts
@@ -1,5 +1,27 @@
-async function saveAnswer(answer: any): Promise<any> {
+import { PrismaClient } from '@prisma/client'
+
+
+async function saveAnswer(answer: any, tableName: string): Promise<any> {
     // Implementar a lógica para salvar a resposta no banco de dados
+
+    const prisma = new PrismaClient();
+    try {
+        // Conectar ao banco de dados
+        await prisma.$connect();
+
+        // Criar um novo usuário usando o objeto padrão
+        const newUser = await prisma[tableName].create({
+            data: answer,
+        });
+        console.log('Novo usuário criado:', newUser);
+
+    } catch (error) {
+        console.error('Ocorreu um erro ao criar o usuário:', error);
+
+    } finally {
+        // Desconectar do banco de dados
+        await prisma.$disconnect();
+    }
 }
 
 async function getAllAnswers(): Promise<any> {
@@ -11,10 +33,32 @@ async function getAllAnswers(): Promise<any> {
 async function getAnswerById(id: string): Promise<any> {
     // Implementar a lógica para obter uma resposta específica do banco de dados
 }
+async function getTable(tableName: string) {
+    const prisma = new PrismaClient();
+  
+    try {
+        // Conectar ao banco de dados
+        await prisma.$connect();
+  
+        // Consultar todas as linhas da tabela desejada
+        const tableRows = await prisma[tableName].findMany();
+  
+        // Imprimir as linhas da tabelate
+        console.log('Linhas da tabela:');
+        console.table(tableRows);
+
+    } catch (error) {
+        console.error('Ocorreu um erro:', error);
+
+    } finally {
+        // Desconectar do banco de dados
+        await prisma.$disconnect();
+    }
+  }
 
 export const answersRepository = {
     saveAnswer,
     getAllAnswers,
-    getAnswerById
+    getAnswerById,
+    getTable
 }
-
