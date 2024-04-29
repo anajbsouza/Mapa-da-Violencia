d45be108b5aa392ff0222ae4ef6f2d5257836662
Kec3-Lin3 <keceline74@gmail.com>
2024-04-29 13:18:57 -0300

    authorization-controller page

--- backend/src/controllers/authorization-controller.ts
+++ backend/src/controllers/authorization-controller.ts
@@ -2,7 +2,7 @@ import {Request, Response } from 'express';
 import { authorizationService } from '@/services/authorization-services';
 
 async function getAuthorized (req: Request, res: Response): Promise<void>{
-
+    
 }
 
 async function postAuthorized (req: Request, res: Response): Promise<void>{
