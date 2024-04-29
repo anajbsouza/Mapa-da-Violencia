a59f5cba444ef49677ec02ac0e0585a92fd0cd5b
Kec3-Lin3 <keceline74@gmail.com>
2024-04-29 12:47:13 -0300

    add authorization-controller to fix authorization pages

--- backend/src/controllers/authorization-controller.ts
+++ backend/src/controllers/authorization-controller.ts
@@ -1,5 +1,5 @@
 import {Request, Response } from 'express';
-import { authorizationService } from '@/services/authorization-service';
+import { authorizationService } from '@/services/authorization-services';
 
 async function getAuthorized (req: Request, res: Response): Promise<void>{
 
