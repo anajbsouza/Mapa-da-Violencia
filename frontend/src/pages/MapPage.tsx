bdddf2314d005e23af2a823dde506f33104a79ad
Beatriz <bibi.geane2@gmail.com>
2024-05-01 23:22:16 -0300

    feat: react leaflet configuration

--- frontend/src/pages/MapPage.tsx
+++ frontend/src/pages/MapPage.tsx
@@ -1,14 +1,18 @@
-import Header from "../components/Header";
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
-const MapPage = () => {
-
-    return (
-      <div>
-        <Header />
-        <h1>Mapa em breve</h1>
-      </div>
-    );
-  };
-  
-  export default MapPage;
-  
+function Mapa() {
+  return (
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  )
+}
+export default Mapa;
