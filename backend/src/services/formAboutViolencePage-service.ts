d81d8644283744ea5d795869ee4af1e2b9528535
Izabelle Rivas <izabelle.rivas123@gmail.com>
2024-05-07 14:21:24 -0300

    fix: error messages

--- backend/src/services/formAboutViolencePage-service.ts
+++ backend/src/services/formAboutViolencePage-service.ts
@@ -6,11 +6,11 @@ import { time } from "console";
 async function createAboutViolenceOccur(aboutviolence_json: AboutViolence_json): Promise<any> {
     
     if (!aboutviolence_json.date_violence_s||aboutviolence_json.date_violence_s==null){
-        throw validationError('date of the violence');
+        throw validationError('"Date of the violence"');
     } else if (!aboutviolence_json.agegroup||aboutviolence_json.agegroup==null) {
-        throw validationError('age group');
+        throw validationError('"Age group"');
     } else if (!aboutviolence_json.time_violence_s||aboutviolence_json.time_violence_s==null) {
-        throw validationError('time of the violence');
+        throw validationError('"Time of the violence"');
     }
     const date_violence = await ValidateDate(aboutviolence_json.date_violence_s);
     const time_violence = await ValidateTime(aboutviolence_json.time_violence_s,aboutviolence_json.date_violence_s);
@@ -31,9 +31,9 @@ async function ValidateTime(time_string:string,date_string:string) {
     const today = new Date() //in the future could compare with the submission date
     
     if (isNaN(time.getTime())) {
-        throw validationError('time of the violence');
+        throw validationError('"Time of the violence"');
     } else if (time>today) {
-        throw validationError('time of the violence');
+        throw validationError('"Time of the violence"');
     }
     return time;
 }
@@ -44,16 +44,16 @@ async function ValidateDate(date_string:string) {
     const today = new Date()
 
     if (isNaN(date.getTime())) {
-        throw validationError('date of the violence');
+        throw validationError('"Date of the violence"');
     } else if (date>today) {
-        throw validationError('date of the violence');
+        throw validationError('"Date of the violence"');
     }
 
     // verify if is not february 30
     const date_ps = date.toISOString().slice(0,10)
     
     if (date_string != date_ps){
-        throw validationError('date of the violence');
+        throw validationError('"Date of the violence"');
     }
     return date;
 }
--- backend/src/services/formStatePage-service.ts
+++ backend/src/services/formStatePage-service.ts
@@ -6,13 +6,13 @@ async function createStateOccur(violenceState: ViolenceState): Promise<any> {
 
     const listUfs = StatePageRepository.getListUfs()
     if (!(await listUfs).find(state => state.uf_State == violenceState.uf_state)){
-        throw validationError('State');
+        throw validationError('"State"');
     }
     else if (typeof violenceState.uf_state !== 'string'){
-        throw validationError('State');
+        throw validationError('"State"');
     }
     else if (!violenceState.uf_state||violenceState.uf_state==null){
-        throw validationError('State');
+        throw validationError('"State"');
     }
     const newStateOccur = await StatePageRepository.StateOccurrence(violenceState);
     await StatePageRepository.upd_numOccurrences_StateList(violenceState);
