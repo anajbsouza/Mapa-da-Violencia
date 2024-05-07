f57e8532e27ede38a69afd8f5e67db7429d3b33b
Izabelle Rivas <izabelle.rivas123@gmail.com>
2024-05-07 15:34:36 -0300

    validation of the violenceoptions field
    Co-authored-by: catlenc <catlen.cleane@hotmail.com>

--- backend/src/services/formClassifyViolencePage-service.ts
+++ backend/src/services/formClassifyViolencePage-service.ts
@@ -8,8 +8,20 @@ async function createViolencesSituationsOccur(classifyviolencepage: ClassifyViol
     } else if (classifyviolencepage.violencesoptions == ""){
         throw validationError('"Violence Situations"');
     }
+    await validateViolenceOptions(classifyviolencepage.violencesoptions);
+
     const newInfoOccur = await ClassifyViolencePageRepository.ViolencesSituations(classifyviolencepage);
     return newInfoOccur;
+}
+async function validateViolenceOptions(violencesoptions:string) {
+    const array_violences = violencesoptions.split(",");
+    const aux = ['VS1','VS2','VS3','VS4','VS5','VS6','VS7','VS8','VS9','VS10'];
+    // if (!array_violences.includes(aux)){
+    //     throw validationError('"Violence Situations"');
+    // }
+    if (array_violences.filter(st => !(aux.includes(st))).length != 0){
+        throw validationError('"Violence Situations"');
+    }
 }
 
 export const ClassifyViolencePageService = {
