4ac487fb57960dde41a7fcc39e9b3694e70f89a0
catlenc <catlen.cleane@hotmail.com>
2024-05-23 15:31:44 -0300

    feat: mapFilterPage-services

---
+++ backend/src/services/mapFilterPage-services.ts
@@ -0,0 +1,19 @@
+import { MapFilterRepository } from "@/repositories/mapFilterPage-repository";
+import { authorizationRepository } from "../repositories/authorization-repository";
+import { validationError } from "../errors/errors";
+
+
+async function getViolenceData(id_user_s: string) {
+    const id_user = BigInt(id_user_s);
+    const listUsers = await authorizationRepository.getListUsers()
+    if (!(await listUsers).find(userlist => id_user == userlist.id)){
+        throw validationError('"Id user"');
+    }
+    const violenceData = await MapFilterRepository.getViolenceData(id_user)
+    return violenceData
+}
+
+export const MapPageService = {
+    getViolenceData
+}
+
