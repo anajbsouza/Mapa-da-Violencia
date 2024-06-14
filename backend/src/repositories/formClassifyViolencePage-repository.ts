2f045236b869125f332809a1cf65bd6e3f639be3
catlenc <catlen.cleane@hotmail.com>
2024-06-14 11:38:39 -0300

    fix: backend formClassifyViolencePage
    Co-authored-by: catlenc <catlen.cleane@hotmail.com>
    Co-authored-by: izabellerivas <izabelle.rivas123@gmail.com>

--- backend/src/repositories/formClassifyViolencePage-repository.ts
+++ backend/src/repositories/formClassifyViolencePage-repository.ts
@@ -3,20 +3,20 @@ import { ClassifyViolencePage } from '../protocols';
 
 const prisma = new PrismaClient();
 
-async function ViolencesSituations(violences_situations: ClassifyViolencePage,typeofviolence: string): Promise<occurrence> {
-    const {id_occur,violencesoptions} = violences_situations;
-    const occurrence = await prisma.occurrence.update({
-        where:{
-            id_occurrence: id_occur
-        },
-        data:{
-            violences_options: violencesoptions,
-            violence_type: typeofviolence,
-        }
-    })
-    return occurrence
-}
+// async function ViolencesSituations(violences_situations: ClassifyViolencePage,typeofviolence: string): Promise<occurrence> {
+//     const {id_occur,violencesoptions} = violences_situations;
+//     const occurrence = await prisma.occurrence.update({
+//         where:{
+//             id_occurrence: id_occur
+//         },
+//         data:{
+//             violences_options: violencesoptions,
+//             violence_type: typeofviolence,
+//         }
+//     })
+//     return occurrence
+// }
 
-export const ClassifyViolencePageRepository = {
-    ViolencesSituations
-}
+// export const ClassifyViolencePageRepository = {
+//     ViolencesSituations
+// }
--- backend/src/services/formClassifyViolencePage-service.ts
+++ backend/src/services/formClassifyViolencePage-service.ts
@@ -1,24 +1,21 @@
 import { ClassifyViolencePage } from '../protocols';
 import { repositoryError, validationError } from "../errors/errors";
-import { ClassifyViolencePageRepository } from "../repositories/formClassifyViolencePage-repository";
+//import { ClassifyViolencePageRepository } from "../repositories/formClassifyViolencePage-repository";
 import { authorizationRepository } from "../repositories/authorization-repository";
 
 async function ValidateViolencesSituationsOccur(classifyviolencepage: ClassifyViolencePage) {
     
-    const ListOccur = await authorizationRepository.getListOccur()
-    if (!(await ListOccur).find(occurlist => classifyviolencepage.id_occur == occurlist.id_occurrence)){
-        throw validationError('"Id occur"');
-    } else if (!classifyviolencepage.violencesoptions||classifyviolencepage.violencesoptions==null){
+    if (!classifyviolencepage.violencesoptions||classifyviolencepage.violencesoptions==null){
         throw validationError('"Violence Situations"');
     } else if (classifyviolencepage.violencesoptions == ""){
         throw validationError('"Violence Situations"');
     }
-    const typeofviolence = await IdentifyTypeOfViolence(classifyviolencepage.violencesoptions);
-    try {
-        return await ClassifyViolencePageRepository.ViolencesSituations(classifyviolencepage,typeofviolence);
-    } catch {
-        throw repositoryError('"Occurrence"','"ViolencesSituations"');
-    }
+    return await IdentifyTypeOfViolence(classifyviolencepage.violencesoptions);
+    // try {
+    //     return await ClassifyViolencePageRepository.ViolencesSituations(classifyviolencepage,typeofviolence);
+    // } catch {
+    //     throw repositoryError('"Occurrence"','"ViolencesSituations"');
+    // }
 }
 
 async function IdentifyTypeOfViolence(violencesoptions:string): Promise<string> {
