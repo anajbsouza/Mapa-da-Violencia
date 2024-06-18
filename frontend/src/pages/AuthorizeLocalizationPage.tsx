80499b3664bd11cbb297b316b5d5eb44c7e81819
barbarabea <barbarabeatrizsilva6@gmail.com>
2024-06-18 14:54:26 -0300

    fix: fixing component navigation FormStateOptions

--- frontend/src/pages/AuthorizeLocalizationPage.tsx
+++ frontend/src/pages/AuthorizeLocalizationPage.tsx
@@ -3,6 +3,7 @@ import Header from "../components/Header";
 import '../styles/AuthorizeLocalizationAndEmergencyPages.css';
 import FormIndex from "../components/FormIndex";
 import axios from "axios";
+import { RxCodesandboxLogo } from "react-icons/rx";
 
 const URL = "http://localhost:4000/map-filter";
 
@@ -51,11 +52,16 @@ const AuthorizeLocalizationPage = () => {
   };
 
   const handleNotAuthorize = () => {
+    var formStateValue = ' ';
     if (action === 'viewMap') {
       navigate("/form-state", { state: { action: 'viewMap' } });
+      formStateValue = 'map-view'; 
     } else {
       navigate("/form-state", { state: { action: 'register' } });
+      formStateValue = 'occurrence-record';
     }
+
+    return formStateValue;
   };
 
   return (
@@ -73,7 +79,7 @@ const AuthorizeLocalizationPage = () => {
 
         <section className="buttons-container">
           <button className="authorize" onClick={handleAuthorize}>Autorizo</button>
-          <button className="not-authorize" onClick={handleNotAuthorize}>Não autorizo</button>
+          <button className="not-authorize" onClick={handleNotAuthorize }>Não autorizo</button>
         </section>
       </main>
     </div>
