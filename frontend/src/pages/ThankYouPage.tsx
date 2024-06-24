64b7f3ee5dfe0d761222d90fb874c422b6c2cabf
Beatriz <bibi.geane2@gmail.com>
2024-06-24 14:38:08 -0300

    feat: desktop thankyou-page responsivity

--- frontend/src/pages/ThankYouPage.tsx
+++ frontend/src/pages/ThankYouPage.tsx
@@ -17,18 +17,19 @@ const ThankYouPage = () => {
                     <FormIndex value={4}/>
                 </section>
 
-                <section className="thankyouTitles">
-                    <h2>OBRIGADA!</h2>
-                    <h2>O seu registro ajuda outras mulheres!</h2>
+                <section className="main-knowmore">
+                    <section className="thankyouTitles">
+                        <h3>OBRIGADA!</h3>
+                        <h3>O seu registro ajuda outras mulheres!</h3>
+                    </section>
+
+                    <section className="knowMore-Thankyou">
+                        <TiPencil className="TiPencil" />
+                        <button className="button-know-more" onClick={() => navigate("/violence-types")}>Quero saber mais</button>
+                    </section>
+
+                    <button className="footer" onClick={() => navigate("/home-page")}>Finalizar</button>
                 </section>
-
-                <section className="knowMore-Thankyou">
-                    <TiPencil className="TiPencil" />
-                    <button className="button-know-more" onClick={() => navigate("/violence-types")}>Quero saber mais</button>
-                </section>
-
-                <button className="footer" onClick={() => navigate("/home-page")}>Finalizar</button>
-
             </main>
         </section>
     );
