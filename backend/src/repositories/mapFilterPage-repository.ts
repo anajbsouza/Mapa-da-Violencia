6feb7f1f2b15aa839dd5026baf57af0dd9ee8b3e
Izabelle Rivas <izabelle.rivas123@gmail.com>
2024-07-09 11:59:40 -0300

    feat: get method from map-filter screen with time window

--- backend/src/repositories/mapFilterPage-repository.ts
+++ backend/src/repositories/mapFilterPage-repository.ts
@@ -1,12 +1,25 @@
 import { occurrence, PrismaClient } from '@prisma/client';
+import { date } from 'joi';
 
 const prisma = new PrismaClient();
 
-//  consulta o banco de dados para encontrar dados de violência (latitude, longitude, tipo de violência) associados a um id_user específico usando o método findUnique do Prisma. Em seguida, retorna esses dados.
+//  consulta o banco de dados e retorna todas as ocorrências com date_violence nos últimos 5 anos
+
 async function getViolenceData() {
+    const timeWindow = 5 // years   
+
+    const currentDate = new Date();
+    const dateFilter =  new Date(currentDate);
+    dateFilter.setFullYear(currentDate.getFullYear() - timeWindow)
     const violencedata = await prisma.occurrence.findMany({
-        select: {
-            latitude: true,
+        where: {
+            date_violence:{
+                gt: dateFilter
+            },
+        },
+        select: {       
+            id_occurrence: true,                                                            
+            latitude: true,                 
             longitude: true,
             violence_type: true
         }
