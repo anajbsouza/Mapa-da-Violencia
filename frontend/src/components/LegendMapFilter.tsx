52dd6fd9a1e519798e8643a73ace9eb545df516e
barbarabea <barbarabeatrizsilva6@gmail.com>
2024-07-17 16:17:12 -0400

    adding subtitle open and close

--- frontend/src/components/LegendMapFilter.tsx
+++ frontend/src/components/LegendMapFilter.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import '../styles/LegendMapFilter.css';
 
 const LegendMapFilter: React.FC = () => {
@@ -9,19 +10,30 @@ const LegendMapFilter: React.FC = () => {
       { color: '#008000', label: 'Violência Patrimonial' },
       { color: '#ff0000', label: 'Violência Sexual' },
     ];
+
+    const [isLegendVisible, setIsLegendVisible] = useState(false);
+
   
     return (
-      <div className="legend-container">
-        <h3>Legenda</h3>
-        <ul className="legend-list">
-          {legends.map((legend, index) => (
-            <li key={index}>
-              <span className="legend-color" style={{ backgroundColor: legend.color }}></span>
-              <span className="legend-label">{legend.label}</span>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <section className='legend-container'>
+          <p className="legend-list p" onClick={() => setIsLegendVisible(!isLegendVisible)}>
+            {isLegendVisible ? 'x' : 'Abrir legenda'}
+          </p>
+
+          { isLegendVisible && ( 
+            <div>
+              <h3>Legenda</h3>
+              <ul className="legend-list">
+                {legends.map((legend, index) => (
+                  <li key={index}>
+                    <span className="legend-color" style={{ backgroundColor: legend.color }}></span>
+                    <span className="legend-label">{legend.label}</span>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ) }
+        </section>
     );
   };
   
