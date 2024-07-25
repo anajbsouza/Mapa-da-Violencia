96745b156477671a4f4ecbac12ed8af8040fda1b
Beatriz <bibi.geane2@gmail.com>
2024-07-25 17:35:26 -0300

    feat: docker oficial

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    host: '0.0.0.0',  // Adicione isto para garantir que o Vite escute em todas as interfaces de rede
+  }
 })
