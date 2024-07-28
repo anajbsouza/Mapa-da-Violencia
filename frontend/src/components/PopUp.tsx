37ded7da0fd97676d49237857c90bb640b50a026
Beatriz <bibi.geane2@gmail.com>
2024-07-28 00:31:36 -0300

    feat: new home page popup

--- frontend/src/components/PopUp.tsx
+++ frontend/src/components/PopUp.tsx
@@ -1,12 +1,17 @@
 import '../styles/PopUp.css';
 
-const PopupComponent = ({ onAuthorize, onNotAuthorize }) => {
+interface PopupComponentProps {
+  onAuthorize: () => void;
+  onNotAuthorize: () => void;
+}
+
+const PopupComponent: React.FC<PopupComponentProps> = ({ onAuthorize, onNotAuthorize }) => {
   return (
     <div className="popup-overlay">
       <div className="popup-content">
         <h3>MAPA DA VIOLÊNCIA</h3>
-        <p>O seu registro ajuda a salvar a vida de outras mulheres! </p>
-        <p>Para uma melhor experiência, você autoriza o acesso a sua localização? </p>
+        <p>O seu registro ajuda a salvar a vida de outras mulheres!</p>
+        <p>Para uma melhor experiência, você autoriza o acesso á sua localização?</p>
         <button className="yes-btn" onClick={onAuthorize}>Sim</button>
         <button className="no-btn" onClick={onNotAuthorize}>Não</button>
       </div>
