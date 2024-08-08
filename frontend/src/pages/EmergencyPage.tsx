528be8d97f00c612f28a52444a432baa84506b50
Beatriz-ge <bibi.geane2@gmail.com>
2024-08-08 10:12:22 -0300

    feat: emergency page styles

--- frontend/src/pages/EmergencyPage.tsx
+++ frontend/src/pages/EmergencyPage.tsx
@@ -1,7 +1,10 @@
+import { useEffect, useState } from "react";
 import Header from "../components/Header";
 import '../styles/EmergencyPage.css';
 
 const EmergencyPage = () => {
+    const [alertShown, setAlertShown] = useState(false);
+
     const callPolice = () => {
         window.location.href = 'tel:190';
     };
@@ -10,18 +13,31 @@ const EmergencyPage = () => {
         window.location.href = 'tel:180';
     };
 
+    useEffect(() => {
+        if (!alertShown && window.innerWidth > 768) {
+            setAlertShown(true);
+            setTimeout(() => {
+                alert("Esta funcionalidade está disponível apenas para dispositivos móveis.");
+            }, 0);
+        }
+    }, [alertShown]);
+
     return (
         <section>
             <Header/>
             <main>
-                <section className="question-emergency"> 
-                    <h3>Você está em risco no momento?</h3>
-                    <p>O número 190 é o telefone da Polícia Militar que deve ser acionado em casos de necessidade imediata ou socorro rápido.</p>
-                </section>
+                <section className="question">
 
-                <section className="buttons-container-emergency">
-                    <button className="green-button" onClick={callPolice}>Sim, ligar 190</button>
-                    <button className="red-button" onClick={callWomanService}>Não, realizar uma denúncia</button>
+                    <section className="question"> 
+                        <p className="title-text">Você está em risco no momento?</p>
+                        <p className="middle-text">O número 190 é o telefone da Polícia Militar que deve ser acionado em casos de necessidade imediata ou socorro rápido.</p>
+                    </section>
+
+                    <section className="buttons-container">
+                        <button className="authorize" onClick={callPolice}>Sim, ligar 190</button>
+                        <button className="not-authorize" onClick={callWomanService}>Não, fazer denúncia</button>
+                    </section>
+
                 </section>
             </main>
         </section>
