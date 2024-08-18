f56685df02c29d91cafbaebb74e66031bc8f8b22
Beatriz <bibi.geane2@gmail.com>
2024-08-17 21:48:46 -0300

    fix: heade map component corrections

--- frontend/src/components/HeaderMap.tsx
+++ frontend/src/components/HeaderMap.tsx
@@ -1,18 +1,20 @@
-import Logo from "../assets/logo2.png"
+import Logo from "../assets/logo2.png";
 import { useNavigate } from "react-router-dom";
+import { IoChevronBackCircleSharp } from "react-icons/io5"; 
 
 const HeaderMap = () => {
-    let navigate = useNavigate();
+    const navigate = useNavigate();
 
     return (
         <div>
-            <header className="header">
-                <section className="header-map-items">
-                    <img className="logo-map" src={Logo} alt="Logo da Gloria" onClick={() => navigate("/")}/>
-                </section>
+            <header className="header-map">
+                <button className="button-back-map" onClick={() => navigate(-1)}>
+                    <IoChevronBackCircleSharp className="icon-back-map" />
+                </button>
+                <img className="logo-map" src={Logo} alt="Logo da Gloria" onClick={() => navigate("/")}/>
             </header>
         </div>
     );
 };
 
-export default HeaderMap; 
+export default HeaderMap;
